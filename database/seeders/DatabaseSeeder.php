15f7dda2beed89d2b5835b2fb738bc1076ed6267
DarioNL <darioponzo03@gmail.com>
2021-04-09 19:16:21 +0200

    database type verandert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(FakeUserSeeder::class);
+//        $this->call(FakeUserSeeder::class);
         $this->call(DestinationSeeder::class);
         $this->call(TravelSeeder::class);
         $this->call(AdminSeeder::class);
